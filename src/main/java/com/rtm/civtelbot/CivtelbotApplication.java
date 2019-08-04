c4720a18267fda8658a203ed15b206b5d010d326
Rtm <aa.solonschikov@gmail.com>
2019-08-04 17:44:54 +0300

    Initial commit

---
+++ src/main/java/com/rtm/civtelbot/CivtelbotApplication.java
@@ -0,0 +1,13 @@
+package com.rtm.civtelbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivtelbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CivtelbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rtm/civtelbot/CivtelbotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rtm.civtelbot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CivtelbotApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
